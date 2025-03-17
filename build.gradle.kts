c79d268d25c94398e0571be7e844cd6381c767dd
nardis0321 <nardis0321@gmail.com>
2025-03-17 22:25:39 +0900

    First Commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.poi:poi-ooxml:5.2.5") // Apache POI
+    implementation("com.github.pjfanning:excel-streaming-reader:5.0.2")
+
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
